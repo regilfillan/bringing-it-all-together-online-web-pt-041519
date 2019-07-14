2bfbde1d2a61a237d2f429fa5f222fd04900ac5b
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-14 21:44:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,15 +10,26 @@ class Dog
     @breed= breed
   end
   
-  # def self.create_table
-  #   sql= <<- SQL 
-  #   CREATE TABLE IF NOT EXISTS dogs (
-  #     id INTEGER PRIMARY KEY,
-  #     name TEXT,
-  #     breed TEXT)
-  #     SQL
-  #     DB[:conn].execute(sql)
-  # end 
+  def self.create_table
+    sql= <<- SQL 
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT)
+      SQL
+      DB[:conn].execute(sql)
+  end 
+  
+    def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
+  end
   
   def self.drop_table
     
