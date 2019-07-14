71508e4f9ee18326723f2204af9c655596174f6a
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-14 22:13:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,10 @@ class Dog
   end 
   
   def self.new_from_db
-    
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
   end 
   
   def self.find_by_name
