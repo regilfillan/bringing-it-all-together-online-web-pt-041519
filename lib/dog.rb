b38eea3b9cac01070d56b0f3831fead6424b8da7
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-14 22:44:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,49 +1,52 @@
 require 'pry'
 require_relative "../config/environment.rb"
 
-class Dog 
-  attr_accessor :id, :name, :breed
-  
+class Dog
+
+  attr_accessor :name, :breed, :id
+
   def initialize(id: nil, name:, breed:)
-    @id= id
-    @name= name 
-    @breed= breed
+    @id = id
+    @name = name
+    @breed = breed
   end
-  
+
   def self.create_table
-    sql= <<-SQL 
-    CREATE TABLE IF NOT EXISTS dogs (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      breed TEXT)
-      SQL
-      DB[:conn].execute(sql)
-  end 
-  
-  def self.drop_table
-    sql= "DROP TABLE IF EXISTS dogs"
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+    SQL
     DB[:conn].execute(sql)
-  end 
-  
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+
   def save
     if self.id
       self.update
     else
-      sql= <<-SQL
+      sql = <<-SQL
         INSERT INTO dogs (name, breed)
-        VALUES (?,?)
+        VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.breed)
-      id= DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
     self
-  end 
-  
+  end
+
   def self.create(name:, breed:)
-    dog= Dog.new(name: name, breed: breed)
+    dog = Dog.new(name: name, breed: breed)
     dog.save
     dog
   end
+
   
  def self.find_or_create_by(name:, breed:)
     sql = <<-SQL
