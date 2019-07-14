f7e193188b3bd0d3481860390aa88c0e5ea94c0e
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-14 21:41:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,4 +10,40 @@ class Dog
     @breed= breed
   end
   
+  def self.create_table
+    sql= <<- SQL 
+  end 
+  
+  def self.drop_table
+    
+  end 
+  
+  def save
+    
+  end 
+  
+  def self.create 
+    
+  end
+  
+  def self.find_by_id
+    
+  end 
+  
+  def self.find_or_create_by
+    
+  end 
+  
+  def self.new_from_db
+    
+  end 
+  
+  def self.find_by_name
+    
+  end 
+  
+  def update
+    
+  end
+  
 end 
