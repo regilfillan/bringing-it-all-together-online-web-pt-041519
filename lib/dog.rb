0b56452a6f4c7e340af4c66d10d7046835a4d08d
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-14 22:15:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,34 +45,63 @@ class Dog
     dog
   end
   
-  def self.find_by_id
+ def self.find_or_create_by(name:, breed:)
     sql = <<-SQL
-      SELECT * FROM dogs
-      WHERE id = ? LIMIT 1
-    SQL
+          SELECT *
+          FROM dogs
+          WHERE name = ?
+          AND breed = ?
+          LIMIT 1
+        SQL
 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end.first
-  end 
-  
-  def self.find_or_create_by
-    
-  end 
-  
-  def self.new_from_db
+    dog = DB[:conn].execute(sql,name,breed)
+
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
+  def self.new_from_db(row)
     id = row[0]
     name = row[1]
     breed = row[2]
     self.new(id: id, name: name, breed: breed)
-  end 
-  
-  def self.find_by_name
-    
-  end 
-  
-  def update
-    
   end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
   
 end 
