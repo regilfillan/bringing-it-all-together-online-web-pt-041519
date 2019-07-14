b6e0e62fee7ca03091a0956dcd7193604363451c
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-14 22:12:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,15 +46,14 @@ class Dog
   end
   
   def self.find_by_id(id)
-    sql= <<-SQL
+    sql = <<-SQL
       SELECT * FROM dogs
-      WHERE id= ?
-      LIMIT 1 
-      SQL 
-      
-    DB[:conn].execute(sql, id).map do |row|
-    
-  end
+      WHERE id = ? LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,id).map do |row|
+      self.new_from_db(row)
+    end.first
   end 
   
   def self.find_or_create_by
